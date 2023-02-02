f3edb48bb91b49f4fe55ab80a7701ec66caf3a1c
StaMoraiti <smoraiti1@sheffield.ac.uk>
2023-02-02 17:22:14 +0000

    Treatment classification codes
    Adding the treatment classification codes and the pairwise plot. Find them in the PCA/POSTPROCESSING folder

---
+++ PCA/POSTPROCESSING/InterventionEffect_Classifier.m
@@ -0,0 +1,173 @@
+clear all;
+dir_codes=pwd;
+%% Scores Clustering with respect to the intervention groups and timepoints t1 and t2
+[a_g1,a_g2,a_g3,a_g4]=scores_clustering;
+Mml=size(a_g1,1);Movx=size(a_g3,1);
+%% FIRST CRTERION: Identifies the modes that describe temporal variations
+%% Gradient of the scores between t1 and t2 should be of the
+%% same sign for every sample
+%% if there is one sample not meeting this requirment, then:
+%% 1) create the distribution of the scores gradient
+%% 2) accept the possible outlier only if it belongs in the m+-3std
+%% if there are more than one scores having different sign of gradient then,
+%% the corresponding modes describes natural variability or uncesrtainty
+%% AND NOT A TEMPORAL VARIATION
+
+daML=diff_M(a_g1,a_g2);
+daOVX=diff_M(a_g3,a_g4);
+figure;
+group=[zeros(Mml,1);ones(Movx,1)];
+colors=lines(length(unique(group)));
+cd(strcat(dir_codes,'/pairplot/'))
+pairplot([daML(:,1) daML(:,6);daOVX(:,1) daOVX(:,6)], {'\deltaa_1', '\deltaa_6'}, num2cell(num2str(group)), colors, 'bar')
+flagML=sign(daML);
+flagOVX=sign(daOVX);
+
+sML=sum(flagML,1);
+sOVX=sum(flagOVX,1);
+OVXmode=[];
+
+MLmodeT=Criterion_Gradient(Mml,sML,daML);
+OVXmodeT=Criterion_Gradient(Movx,sOVX,daOVX);
+
+%% SECOND CRITERION: Identifies the treatment related temporal variations by:
+%% 1)keeping the modes that describe temporal changes in the ML group AND
+%% do not show up in the corresponding matrix of the OVX group
+%% 2)keeping the common modes only if the mean absolute scores-gradient is 
+%% higher in the ML than OVX group.
+%  This means that this mode describes a temporal change in both groups but
+%  the intervention accelerates this geometrical change.
+
+%-- OUTPUT--:
+% The index of the modes that satisfy these conditions
+
+TRmode=Criterion_GroupComparison(MLmodeT,OVXmodeT,daML,daOVX);
+
+%% THIRD CRITERION: Identifies the important treatment effects
+%% It applies Wilcoxon paired two sided test to reject the null hypothesis that the 
+%% the medians of the subgroups of scores pre and post treatmennt are not equal
+for q=1:length(TRmode)
+[~,h(q)] = signrank(a_g1(:,TRmode(q)),a_g2(:,TRmode(q)));
+[~,h2(q)] = signrank(a_g3(:,TRmode(q)),a_g4(:,TRmode(q)));
+end
+SSTRmode=TRmode(h>0);
+
+%% FORTH CRITERION: Measures the effect size for eac significant mode
+%% The gropus are the modal scores of the ML groups classified
+%% with respect to the time point
+%% As the aim is to compare two time points , pre and post the intervention
+%% within one group, the effect size formula is the Cohen's d averaged.
+
+for q=1:length(SSTRmode)
+    a_g1I=a_g1(:,SSTRmode(q));a_g2I=a_g2(:,SSTRmode(q));
+    davML(q,1)=Average_CohensD(a_g1I,a_g2I);
+    
+    a_g3I=a_g3(:,SSTRmode(q));a_g4I=a_g4(:,SSTRmode(q));
+    davOVX(q,1)=Average_CohensD(a_g3I,a_g4I);
+
+end
+
+EffectSize={SSTRmode,davML};
+
+% Comparison between control and intervention group
+Flag2=abs(davOVX(:))<abs(davML(:));
+
+%% EXTRA ATTRIBUTE 1: The std of the modal scores for the ML and t2 samples
+%% is higher than the std of the corresponding score of the OVX.
+
+Flag3=std(a_g1(:,SSTRmode))>std(a_g3(:,SSTRmode));
+
+
+%% EXTRA ATTRIBUTE 2: percent variation explained form the significant 
+%% treatment related modes.
+load('../../OUTPUTS/PCA_results_MLvsOVX_Registered.mat','latent','coeff','score')
+cum_var=cumsum(latent)/sum(latent);
+for i=1:size(SSTRmode,1)
+    if i==1
+        Mvar(i)=cum_var(SSTRmode(i));
+    else
+        Mvar(i)=cum_var(SSTRmode(i))-cum_var(SSTRmode(i)-1);
+    end
+end
+
+%% EXTRA ARRTIBUTE 3: Max localized magnitude of the averaged temporal mode change
+for i=1:size(SSTRmode,1)
+    LocMCha(i)=abs(max(coeff(:,SSTRmode(i)))*mean(daML(:,SSTRmode(i))));
+end
+
+
+
+
+
+%% Supplementary functions
+
+function dav=Average_CohensD(g1,g2)
+Adiff=g1-g2;
+s1=std(g1);s2=std(g2);
+
+Stdav = (s1 + s2)/2;
+dav = mean(Adiff) / Stdav;
+
+
+
+end
+
+
+function A=Criterion_GroupComparison(modesG1,modesG2,daG1,daG2)
+A=[];
+for ii=1:size(modesG1)
+    if isempty(find(modesG2(:,1)==modesG1(ii,1)))
+        A=[A;modesG1(ii,1)];
+    else
+       if  abs(mean(daG1(:,ii)))>abs(mean(daG2(:,ii)))
+        A=[A;modesG1(ii,1)];
+       else
+           continue;
+       end
+
+    end
+end
+
+
+
+end
+
+function mode=Criterion_Gradient(M,s,da)
+mode=[];
+for k=1:size(s,2)
+    if abs(s(k))>=M-2
+        if abs(s(k))==M
+            mode=[mode;k];
+        else % Mml-1
+           if sign(s(:,k))==-1
+               [~,ind]=max(da(:,k));
+           elseif sign(s(:,k))==1
+               [~,ind]=min(da(:,k));
+           end
+           da_m=mean([da(1:ind-1,k); da(ind+1:end,k)]);
+           da_s=std([da(1:ind-1,k); da(ind+1:end,k)]);
+           if abs(da(ind,k))<abs(da_m)+3*da_s && abs(da(ind,k))>abs(da_m)-3*da_s% express this threshold defined by the distribution 
+              mode=[mode;k];
+           else
+              continue
+           end
+        end
+    end
+end
+
+end
+
+
+function [daN]=diff_M(a_g1,a_g2)
+Ma_i=max([abs(a_g1) ; abs(a_g2)]);
+for i=1:size(a_g1,1)
+    daN(i,:)=(a_g2(i,:)-a_g1(i,:))./Ma_i;  
+    
+    
+end
+
+end
+
+
+
+
---
+++ PCA/POSTPROCESSING/scores_clustering.m
@@ -0,0 +1,194 @@
+function [a_g1,a_g2,a_g3,a_g4]=scores_clustering
+
+Respath='G:\My Drive\PhD\WORK\2nd_year\PCA_PIPELINE\PCA\OUTPUTS\';
+cd(Respath);
+load("PCA_results_MLvsOVX_Registered.mat")
+Inputpath='G:\My Drive\PhD\WORK\2nd_year\PCA_PIPELINE\PCA\MAIN\';
+cd(Inputpath)
+load("PCAinput_MLvsOVX.mat")
+score=-score;
+a_g1=score(1:6,:);muA_g1=mean(a_g1);
+a_g2=score(7:12,:);muA_g2=mean(a_g2);
+a_g3=score(13:17,:);muA_g3=mean(a_g3);
+a_g4=score(18:22,:);muA_g4=mean(a_g4);
+
+g1=[1 1 1 1 1 1];
+g2=[2 2 2 2 2 2];
+g3=[3 3 3 3 3];
+g4=[4 4 4 4 4];
+
+[MaxCoef,Loc3D]=max(abs(coeff(:,1:7)));
+plot_scores(a_g1,a_g2,a_g3,a_g4,g1,g2,g3,g4);
+
+end
+
+function plot_scores(a_g1,a_g2,a_g3,a_g4,g1,g2,g3,g4)
+figure()
+plot(g1,a_g1(:,1),'go');hold on;
+plot(g2,a_g2(:,1),'ro');hold on;
+plot(g3,a_g3(:,1),'go');hold on;
+plot(g4,a_g4(:,1),'ro');hold on;
+t1_2=[1 2];
+a1m1t1_2=[a_g1(1,1) a_g2(1,1)];a1m2t1_2=[a_g1(2,1) a_g2(2,1)];a1m3t1_2=[a_g1(3,1) a_g2(3,1)];
+a1m4t1_2=[a_g1(4,1) a_g2(4,1)];a1m5t1_2=[a_g1(5,1) a_g2(5,1)];a1m6t1_2=[a_g1(6,1) a_g2(6,1)];
+plot(t1_2,a1m1t1_2,'b-');hold on;text(t1_2,a1m1t1_2,'S1');
+plot(t1_2,a1m2t1_2,'b-');hold on;text(t1_2,a1m2t1_2,'S2');
+plot(t1_2,a1m3t1_2,'b-');hold on;text(t1_2,a1m3t1_2,'S3');
+plot(t1_2,a1m4t1_2,'b-');hold on;text(t1_2,a1m4t1_2,'S4');
+plot(t1_2,a1m5t1_2,'b-');hold on;text(t1_2,a1m5t1_2,'S5');
+plot(t1_2,a1m6t1_2,'b-');hold on;text(t1_2,a1m6t1_2,'S6');
+t3_4=[3 4];
+a1m1t3_4=[a_g3(1,1) a_g4(1,1)];a1m2t3_4=[a_g3(2,1) a_g4(2,1)];a1m3t3_4=[a_g3(3,1) a_g4(3,1)];
+a1m4t3_4=[a_g3(4,1) a_g4(4,1)];a1m5t3_4=[a_g3(5,1) a_g4(5,1)];
+plot(t3_4,a1m1t3_4,'b-');hold on;text(t3_4,a1m1t3_4,'S1');
+plot(t3_4,a1m2t3_4,'b-');hold on;text(t3_4,a1m2t3_4,'S2');
+plot(t3_4,a1m3t3_4,'b-');hold on;text(t3_4,a1m3t3_4,'S3');
+plot(t3_4,a1m4t3_4,'b-');hold on;text(t3_4,a1m4t3_4,'S4');
+plot(t3_4,a1m5t3_4,'b-');text(t3_4,a1m5t3_4,'S5');
+title('1st Mode Shape')
+figure()
+plot(g1,a_g1(:,2),'go');hold on;
+plot(g2,a_g2(:,2),'ro');hold on;
+plot(g3,a_g3(:,2),'go');hold on;
+plot(g4,a_g4(:,2),'ro');hold on;
+t1_2=[1 2];
+a2m1t1_2=[a_g1(1,2) a_g2(1,2)];a2m2t1_2=[a_g1(2,2) a_g2(2,2)];a2m3t1_2=[a_g1(3,2) a_g2(3,2)];
+a2m4t1_2=[a_g1(4,2) a_g2(4,2)];a2m5t1_2=[a_g1(5,2) a_g2(5,2)];a2m6t1_2=[a_g1(6,2) a_g2(6,2)];
+plot(t1_2,a2m1t1_2,'b-');hold on;text(t1_2,a2m1t1_2,'S1');
+plot(t1_2,a2m2t1_2,'b-');hold on;text(t1_2,a2m2t1_2,'S2');
+plot(t1_2,a2m3t1_2,'b-');hold on;text(t1_2,a2m3t1_2,'S3');
+plot(t1_2,a2m4t1_2,'b-');hold on;text(t1_2,a2m4t1_2,'S4');
+plot(t1_2,a2m5t1_2,'b-');hold on;text(t1_2,a2m5t1_2,'S5');
+plot(t1_2,a2m6t1_2,'b-');hold on;text(t1_2,a2m6t1_2,'S6');
+t3_4=[3 4];
+a2m1t3_4=[a_g3(1,2) a_g4(1,2)];a2m2t3_4=[a_g3(2,2) a_g4(2,2)];a2m3t3_4=[a_g3(3,2) a_g4(3,2)];
+a2m4t3_4=[a_g3(4,2) a_g4(4,2)];a2m5t3_4=[a_g3(5,2) a_g4(5,2)];
+plot(t3_4,a2m1t3_4,'b-');hold on;text(t3_4,a2m1t3_4,'S1');
+plot(t3_4,a2m2t3_4,'b-');hold on;text(t3_4,a2m2t3_4,'S2');
+plot(t3_4,a2m3t3_4,'b-');hold on;text(t3_4,a2m3t3_4,'S3');
+plot(t3_4,a2m4t3_4,'b-');hold on;text(t3_4,a2m4t3_4,'S4');
+plot(t3_4,a2m5t3_4,'b-');text(t3_4,a2m5t3_4,'S5');
+title('2nd Mode Shape')
+figure()
+plot(g1,a_g1(:,3),'go');hold on;
+plot(g2,a_g2(:,3),'ro');hold on;
+plot(g3,a_g3(:,3),'go');hold on;
+plot(g4,a_g4(:,3),'ro');
+t1_2=[1 2];
+a3m1t1_2=[a_g1(1,3) a_g2(1,3)];a3m2t1_2=[a_g1(2,3) a_g2(2,3)];a3m3t1_2=[a_g1(3,3) a_g2(3,3)];
+a3m4t1_2=[a_g1(4,3) a_g2(4,3)];a3m5t1_2=[a_g1(5,3) a_g2(5,3)];a3m6t1_2=[a_g1(6,3) a_g2(6,3)];
+plot(t1_2,a3m1t1_2,'b-');hold on;text(t1_2,a3m1t1_2,'S1');
+plot(t1_2,a3m2t1_2,'b-');hold on;text(t1_2,a3m2t1_2,'S2');
+plot(t1_2,a3m3t1_2,'b-');hold on;text(t1_2,a3m3t1_2,'S3');
+plot(t1_2,a3m4t1_2,'b-');hold on;text(t1_2,a3m4t1_2,'S4');
+plot(t1_2,a3m5t1_2,'b-');hold on;text(t1_2,a3m5t1_2,'S5');
+plot(t1_2,a3m6t1_2,'b-');hold on;text(t1_2,a3m6t1_2,'S6');
+t3_4=[3 4];
+a3m1t3_4=[a_g3(1,3) a_g4(1,3)];a3m2t3_4=[a_g3(2,3) a_g4(2,3)];a3m3t3_4=[a_g3(3,3) a_g4(3,3)];
+a3m4t3_4=[a_g3(4,3) a_g4(4,3)];a3m5t3_4=[a_g3(5,3) a_g4(5,3)];
+plot(t3_4,a3m1t3_4,'b-');hold on;text(t3_4,a3m1t3_4,'S1');
+plot(t3_4,a3m2t3_4,'b-');hold on;text(t3_4,a3m2t3_4,'S2');
+plot(t3_4,a3m3t3_4,'b-');hold on;text(t3_4,a3m3t3_4,'S3');
+plot(t3_4,a3m4t3_4,'b-');hold on;text(t3_4,a3m4t3_4,'S4');
+plot(t3_4,a3m5t3_4,'b-');text(t3_4,a3m5t3_4,'S5');
+title('3rd Mode Shape')
+figure()
+plot(g1,a_g1(:,4),'go');hold on;
+plot(g2,a_g2(:,4),'ro');hold on;
+plot(g3,a_g3(:,4),'go');hold on;
+plot(g4,a_g4(:,4),'ro');
+t1_2=[1 2];
+a4m1t1_2=[a_g1(1,4) a_g2(1,4)];a4m2t1_2=[a_g1(2,4) a_g2(2,4)];a4m3t1_2=[a_g1(3,4) a_g2(3,4)];
+a4m4t1_2=[a_g1(4,4) a_g2(4,4)];a4m5t1_2=[a_g1(5,4) a_g2(5,4)];a4m6t1_2=[a_g1(6,4) a_g2(6,4)];
+plot(t1_2,a4m1t1_2,'b-');hold on;text(t1_2,a4m1t1_2,'S1');
+plot(t1_2,a4m2t1_2,'b-');hold on;text(t1_2,a4m2t1_2,'S2');
+plot(t1_2,a4m3t1_2,'b-');hold on;text(t1_2,a4m3t1_2,'S3');
+plot(t1_2,a4m4t1_2,'b-');hold on;text(t1_2,a4m4t1_2,'S4');
+plot(t1_2,a4m5t1_2,'b-');hold on;text(t1_2,a4m5t1_2,'S5');
+plot(t1_2,a4m6t1_2,'b-');hold on;text(t1_2,a4m6t1_2,'S6');
+t3_4=[3 4];
+a4m1t3_4=[a_g3(1,4) a_g4(1,4)];a4m2t3_4=[a_g3(2,4) a_g4(2,4)];a4m3t3_4=[a_g3(3,4) a_g4(3,4)];
+a4m4t3_4=[a_g3(4,4) a_g4(4,4)];a4m5t3_4=[a_g3(5,4) a_g4(5,4)];
+plot(t3_4,a4m1t3_4,'b-');hold on;text(t3_4,a4m1t3_4,'S1');
+plot(t3_4,a4m2t3_4,'b-');hold on;text(t3_4,a4m2t3_4,'S2');
+plot(t3_4,a4m3t3_4,'b-');hold on;text(t3_4,a4m3t3_4,'S3');
+plot(t3_4,a4m4t3_4,'b-');hold on;text(t3_4,a4m4t3_4,'S4');
+plot(t3_4,a4m5t3_4,'b-');text(t3_4,a4m5t3_4,'S5');
+title('4th Mode Shape')
+figure()
+plot(g1,a_g1(:,5),'go');hold on;
+plot(g2,a_g2(:,5),'ro');hold on;
+plot(g3,a_g3(:,5),'go');hold on;
+plot(g4,a_g4(:,5),'ro');
+t1_2=[1 2];
+a5m1t1_2=[a_g1(1,5) a_g2(1,5)];a5m2t1_2=[a_g1(2,5) a_g2(2,5)];a5m3t1_2=[a_g1(3,5) a_g2(3,5)];
+a5m4t1_2=[a_g1(4,5) a_g2(4,5)];a5m5t1_2=[a_g1(5,5) a_g2(5,5)];a5m6t1_2=[a_g1(6,5) a_g2(6,5)];
+plot(t1_2,a5m1t1_2,'b-');hold on;text(t1_2,a5m1t1_2,'S1');
+plot(t1_2,a5m2t1_2,'b-');hold on;text(t1_2,a5m2t1_2,'S2');
+plot(t1_2,a5m3t1_2,'b-');hold on;text(t1_2,a5m3t1_2,'S3');
+plot(t1_2,a5m4t1_2,'b-');hold on;text(t1_2,a5m4t1_2,'S4');
+plot(t1_2,a5m5t1_2,'b-');hold on;text(t1_2,a5m5t1_2,'S5');
+plot(t1_2,a5m6t1_2,'b-');hold on;text(t1_2,a5m6t1_2,'S6');
+t3_4=[3 4];
+a5m1t3_4=[a_g3(1,5) a_g4(1,5)];a5m2t3_4=[a_g3(2,5) a_g4(2,5)];a5m3t3_4=[a_g3(3,5) a_g4(3,5)];
+a5m4t3_4=[a_g3(4,5) a_g4(4,5)];a5m5t3_4=[a_g3(5,5) a_g4(5,5)];
+plot(t3_4,a5m1t3_4,'b-');hold on;text(t3_4,a5m1t3_4,'S1');
+plot(t3_4,a5m2t3_4,'b-');hold on;text(t3_4,a5m2t3_4,'S2');
+plot(t3_4,a5m3t3_4,'b-');hold on;text(t3_4,a5m3t3_4,'S3');
+plot(t3_4,a5m4t3_4,'b-');hold on;text(t3_4,a5m4t3_4,'S4');
+plot(t3_4,a5m5t3_4,'b-');text(t3_4,a5m5t3_4,'S5');
+title('5th Mode Shape')
+figure()
+plot(g1,a_g1(:,6),'go');hold on;
+plot(g2,a_g2(:,6),'ro');hold on;
+plot(g3,a_g3(:,6),'go');hold on;
+plot(g4,a_g4(:,6),'ro');
+t1_2=[1 2];
+a6m1t1_2=[a_g1(1,6) a_g2(1,6)];a6m2t1_2=[a_g1(2,6) a_g2(2,6)];a6m3t1_2=[a_g1(3,6) a_g2(3,6)];
+a6m4t1_2=[a_g1(4,6) a_g2(4,6)];a6m5t1_2=[a_g1(5,6) a_g2(5,6)];a6m6t1_2=[a_g1(6,6) a_g2(6,6)];
+plot(t1_2,a6m1t1_2,'b-');hold on;text(t1_2,a6m1t1_2,'S1');
+plot(t1_2,a6m2t1_2,'b-');hold on;text(t1_2,a6m2t1_2,'S2');
+plot(t1_2,a6m3t1_2,'b-');hold on;text(t1_2,a6m3t1_2,'S3');
+plot(t1_2,a6m4t1_2,'b-');hold on;text(t1_2,a6m4t1_2,'S4');
+plot(t1_2,a6m5t1_2,'b-');hold on;text(t1_2,a6m5t1_2,'S5');
+plot(t1_2,a6m6t1_2,'b-');hold on;text(t1_2,a6m6t1_2,'S6');
+t3_4=[3 4];
+a6m1t3_4=[a_g3(1,6) a_g4(1,6)];a6m2t3_4=[a_g3(2,6) a_g4(2,6)];a6m3t3_4=[a_g3(3,6) a_g4(3,6)];
+a6m4t3_4=[a_g3(4,6) a_g4(4,6)];a6m5t3_4=[a_g3(5,6) a_g4(5,6)];
+plot(t3_4,a6m1t3_4,'b-');hold on;text(t3_4,a6m1t3_4,'S1');
+plot(t3_4,a6m2t3_4,'b-');hold on;text(t3_4,a6m2t3_4,'S2');
+plot(t3_4,a6m3t3_4,'b-');hold on;text(t3_4,a6m3t3_4,'S3');
+plot(t3_4,a6m4t3_4,'b-');hold on;text(t3_4,a6m4t3_4,'S4');
+plot(t3_4,a6m5t3_4,'b-');text(t3_4,a6m5t3_4,'S5');
+title('6th Mode Shape')
+figure()
+plot(g1,a_g1(:,7),'go');hold on;
+plot(g2,a_g2(:,7),'ro');hold on;
+plot(g3,a_g3(:,7),'go');hold on;
+plot(g4,a_g4(:,7),'ro');
+t1_2=[1 2];
+a7m1t1_2=[a_g1(1,7) a_g2(1,7)];a7m2t1_2=[a_g1(2,7) a_g2(2,7)];a7m3t1_2=[a_g1(3,7) a_g2(3,7)];
+a7m4t1_2=[a_g1(4,7) a_g2(4,7)];a7m5t1_2=[a_g1(5,7) a_g2(5,7)];a7m6t1_2=[a_g1(6,7) a_g2(6,7)];
+plot(t1_2,a7m1t1_2,'b-');hold on;text(t1_2,a7m1t1_2,'S1');
+plot(t1_2,a7m2t1_2,'b-');hold on;text(t1_2,a7m2t1_2,'S2');
+plot(t1_2,a7m3t1_2,'b-');hold on;text(t1_2,a7m3t1_2,'S3');
+plot(t1_2,a7m4t1_2,'b-');hold on;text(t1_2,a7m4t1_2,'S4');
+plot(t1_2,a7m5t1_2,'b-');hold on;text(t1_2,a7m5t1_2,'S5');
+plot(t1_2,a7m6t1_2,'b-');hold on;text(t1_2,a7m6t1_2,'S6');
+t3_4=[3 4];
+a7m1t3_4=[a_g3(1,7) a_g4(1,7)];a7m2t3_4=[a_g3(2,7) a_g4(2,7)];a7m3t3_4=[a_g3(3,7) a_g4(3,7)];
+a7m4t3_4=[a_g3(4,7) a_g4(4,7)];a7m5t3_4=[a_g3(5,7) a_g4(5,7)];
+plot(t3_4,a7m1t3_4,'b-');hold on;text(t3_4,a7m1t3_4,'S1');
+plot(t3_4,a7m2t3_4,'b-');hold on;text(t3_4,a7m2t3_4,'S2');
+plot(t3_4,a7m3t3_4,'b-');hold on;text(t3_4,a7m3t3_4,'S3');
+plot(t3_4,a7m4t3_4,'b-');hold on;text(t3_4,a7m4t3_4,'S4');
+plot(t3_4,a7m5t3_4,'b-');text(t3_4,a7m5t3_4,'S5');
+title('7th Mode Shape')
+% figure();
+% plot3(a_g1(:,1),a_g1(:,2),a_g1(:,4),'ro');hold on;
+% plot3(a_g2(:,1),a_g2(:,2),a_g2(:,4),'go');hold on;
+% plot3(a_g3(:,1),a_g3(:,2),a_g3(:,4),'bo');hold on;
+% plot3(a_g4(:,1),a_g4(:,2),a_g4(:,4),'ko');
+% xlabel('mode 1');
+% ylabel('mode 2');
+end
